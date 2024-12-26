a7030fe0a34321ea7b0f42cbe5fc0be6443bc061
Jose Villa <jose.villa@version1.com>
2024-12-26 09:02:39 +0100

    Add project files.

---
+++ 2015/01/Program.cs
@@ -0,0 +1,30 @@
+﻿var input = File.ReadAllTextAsync("Input.txt").Result;
+
+var result = input.Count(c => c == '(') - input.Count(c => c == ')');
+
+Console.WriteLine(result);
+
+// Par 2
+int currentFloor = 0;
+int position = 0;
+
+foreach (var c in input)
+{
+	position++;
+
+	if (c == '(')
+	{
+		currentFloor++;
+	}
+	else
+	{
+		currentFloor--;
+	}
+
+	if (currentFloor == -1)
+	{
+		break;
+	}
+}
+
+Console.WriteLine(position);
