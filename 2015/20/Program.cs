bf2a6b406ccf9d1aafa5f4d2fcf553c0c2772f83
Jose Villa <jose.villa@version1.com>
2024-12-27 13:16:52 +0100

    Solved 2015-20

---
+++ 2015/20/Program.cs
@@ -0,0 +1,51 @@
+﻿int target = 29000000;
+
+var presents = CalculatePresents(1000000, 10);
+
+var house = GetHouse();
+
+Console.WriteLine(house);
+
+// Part 2
+presents = CalculatePresents(50, 11);
+
+house = GetHouse();
+
+Console.WriteLine(house);
+
+
+int[] CalculatePresents(int maxVisits, int presentsPerHouse)
+{
+	var presents = new int[1000000];
+
+	for (var i = 1; i < presents.Length; i++)
+	{
+		var j = i;
+		var step = 0;
+
+		while (j < presents.Length &&
+			step < maxVisits)
+		{
+			presents[j] += presentsPerHouse * i;
+
+			j += i;
+
+			step++;
+		}
+	}
+
+	return presents;
+}
+
+int GetHouse()
+{
+	for (var i = 0; i < presents.Length; i++)
+	{
+		if (presents[i] >= target)
+		{
+			return i;
+		}
+	}
+
+	return -1;
+}
