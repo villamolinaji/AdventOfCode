6840364fe39e18c222d1c783b0c302ab766aa37b
Jose Villa <jose.villa@version1.com>
2024-12-27 08:42:58 +0100

    Solved 2015-14

---
+++ 2015/14/Program.cs
@@ -0,0 +1,98 @@
+﻿using _14;
+
+var lines = File.ReadAllLinesAsync("Input.txt").Result;
+
+var reindeers = new List<Reindeer>();
+
+const int TotalSeconds = 2503;
+
+foreach (var line in lines)
+{
+	var parts = line.Split(' ');
+	var name = parts[0];
+	var speed = parts[3];
+	var flyseconds = parts[6];
+	var restseconds = parts[13];
+
+	reindeers.Add(new Reindeer
+	{
+		Name = name,
+		Speed = int.Parse(speed),
+		FlySeconds = int.Parse(flyseconds),
+		RestSeconds = int.Parse(restseconds),
+		Distance = 0,
+		CurrentFly = 0,
+		CurrentRest = 0,
+		Points = 0
+	});
+}
+
+for (int i = 0; i < TotalSeconds; i++)
+{
+	foreach (var reindeer in reindeers)
+	{
+		if (reindeer.CurrentFly < reindeer.FlySeconds)
+		{
+			reindeer.Distance += reindeer.Speed;
+			reindeer.CurrentFly++;
+		}
+		else if (reindeer.CurrentRest < reindeer.RestSeconds)
+		{
+			reindeer.CurrentRest++;
+
+			if (reindeer.CurrentRest == reindeer.RestSeconds)
+			{
+				reindeer.CurrentFly = 0;
+				reindeer.CurrentRest = 0;
+			}
+		}
+	}
+}
+
+var maxDistance = reindeers.Max(r => r.Distance);
+
+Console.WriteLine(maxDistance);
+
+
+// Part 2
+foreach (var reindeer in reindeers)
+{
+	reindeer.Distance = 0;
+	reindeer.CurrentFly = 0;
+	reindeer.CurrentRest = 0;
+}
+
+for (int i = 0; i < TotalSeconds; i++)
+{
+	foreach (var reindeer in reindeers)
+	{
+		if (reindeer.CurrentFly < reindeer.FlySeconds)
+		{
+			reindeer.Distance += reindeer.Speed;
+			reindeer.CurrentFly++;
+		}
+		else if (reindeer.CurrentRest < reindeer.RestSeconds)
+		{
+			reindeer.CurrentRest++;
+
+			if (reindeer.CurrentRest == reindeer.RestSeconds)
+			{
+				reindeer.CurrentFly = 0;
+				reindeer.CurrentRest = 0;
+			}
+		}
+	}
+
+	var currentMaxDistance = reindeers.Max(r => r.Distance);
+	var winningReindeers = reindeers.Where(r => r.Distance == currentMaxDistance);
+
+	foreach (var winner in winningReindeers)
+	{
+		winner.Points++;
+	}
+}
+
+var maxPoints = reindeers.Max(r => r.Points);
+
+Console.WriteLine(maxPoints);
+
---
+++ 2015/14/Reindeer.cs
@@ -0,0 +1,21 @@
+﻿namespace _14
+{
+	internal class Reindeer
+	{
+		required public string Name { get; set; }
+
+		required public int Speed { get; set; }
+
+		required public int FlySeconds { get; set; }
+
+		required public int RestSeconds { get; set; }
+
+		required public int Distance { get; set; }
+
+		required public int CurrentFly { get; set; }
+
+		required public int CurrentRest { get; set; }
+
+		required public int Points { get; set; }
+	}
+}
