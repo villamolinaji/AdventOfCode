8c2d6250ea864367a99b92f10a78a3eb27d89fea
Jose Villa <jose.villa@version1.com>
2024-12-27 16:53:42 +0100

    Solved 2015-24

---
+++ 2015/24/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Immutable;
+
+var lines = File.ReadAllLinesAsync("Input.txt").Result;
+
+var packages = lines.Select(int.Parse).ToArray();
+
+var result = Solve(3);
+
+Console.WriteLine(result);
+
+// Part 2
+result = Solve(4);
+
+Console.WriteLine(result);
+
+
+long Solve(int groups)
+{
+	for (var i = 0; i < packages.Length; i++)
+	{
+		var solutionPackages = PickPackages(i, 0, packages.Sum() / groups);
+
+		if (solutionPackages.Any())
+		{
+			return solutionPackages.Select(p => p.Aggregate(1L, (m, x) => m * x)).Min();
+		}
+	}
+
+	return 0;
+}
+
+IEnumerable<ImmutableList<int>> PickPackages(int count, int packageIndex, int packagesSumPerGroup)
+{
+	if (packagesSumPerGroup == 0)
+	{
+		yield return ImmutableList.Create<int>();
+		yield break;
+	}
+
+	if (count < 0 ||
+		packagesSumPerGroup < 0 ||
+		packageIndex >= packages.Length)
+	{
+		yield break;
+	}
+
+	if (packages[packageIndex] <= packagesSumPerGroup)
+	{
+		foreach (var x in PickPackages(count - 1, packageIndex + 1, packagesSumPerGroup - packages[packageIndex]))
+		{
+			yield return x.Add(packages[packageIndex]);
+		}
+	}
+
+	foreach (var x in PickPackages(count, packageIndex + 1, packagesSumPerGroup))
+	{
+		yield return x;
+	}
+}
