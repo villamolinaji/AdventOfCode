99e8ffb5d888aafdc9c463384207ba8a247860cd
Jose Villa <jose.villa@version1.com>
2024-12-30 11:56:24 +0100

    2016-02

---
+++ 2016/06/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+var lines = File.ReadAllLinesAsync("Input.txt").Result;
+
+var correctedMessage = new StringBuilder();
+
+var cols = lines[0].Length;
+
+for (var c = 0; c < cols; c++)
+{
+	var line = lines.Select(l => l[c]);
+
+	var characterMostFrequent = line
+		.GroupBy(c => c)
+		.OrderByDescending(g => g.Count())
+		.First()
+		.Key;
+
+	correctedMessage.Append(characterMostFrequent);
+}
+
+Console.WriteLine(correctedMessage.ToString());
+
+// Part 2
+correctedMessage = new StringBuilder();
+
+for (var c = 0; c < cols; c++)
+{
+	var line = lines.Select(l => l[c]);
+
+	var characterMostFrequent = line
+		.GroupBy(c => c)
+		.OrderBy(g => g.Count())
+		.First()
+		.Key;
+
+	correctedMessage.Append(characterMostFrequent);
+}
+
+Console.WriteLine(correctedMessage.ToString());
