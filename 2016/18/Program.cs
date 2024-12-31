dfde58762a64972d1247d8fbab4bcebf8be8c748
Jose Villa <jose.villa@version1.com>
2024-12-31 12:12:09 +0100

    2016-18

---
+++ 2016/18/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+
+var firstRow = "^^.^..^.....^..^..^^...^^.^....^^^.^.^^....^.^^^...^^^^.^^^^.^..^^^^.^^.^.^.^.^.^^...^^..^^^..^.^^^^";
+
+var totalRows = 40;
+
+Console.WriteLine(GetSafeTiles());
+
+// Part 2
+totalRows = 400000;
+
+Console.WriteLine(GetSafeTiles());
+
+
+long GetSafeTiles()
+{
+	var rows = new List<string> { firstRow };
+
+	while (rows.Count < totalRows)
+	{
+		var lastRow = rows[rows.Count - 1];
+		var newRow = new StringBuilder();
+
+		for (var i = 0; i < lastRow.Length; i++)
+		{
+			var left = i == 0
+				? '.'
+				: lastRow[i - 1];
+
+			var center = lastRow[i];
+
+			var right = i == lastRow.Length - 1
+				? '.'
+				: lastRow[i + 1];
+
+			if (left == '^' && center == '^' && right == '.')
+			{
+				newRow.Append('^');
+			}
+			else if (left == '.' && center == '^' && right == '^')
+			{
+				newRow.Append('^');
+			}
+			else if (left == '^' && center == '.' && right == '.')
+			{
+				newRow.Append('^');
+			}
+			else if (left == '.' && center == '.' && right == '^')
+			{
+				newRow.Append('^');
+			}
+			else
+			{
+				newRow.Append('.');
+			}
+		}
+
+		rows.Add(newRow.ToString());
+	}
+
+	return rows.Sum(c => c.Count(c => c == '.'));
+}
