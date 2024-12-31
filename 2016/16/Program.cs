585d518447bdd0abce9e924884f4a1efc8c69659
Jose Villa <jose.villa@version1.com>
2024-12-31 11:32:33 +0100

    2016-16

---
+++ 2016/16/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+
+var input = "11100010111110100";
+var diskLength = 272;
+
+var dragonCurve = GetDragonCurve();
+
+dragonCurve = dragonCurve.Substring(0, diskLength);
+
+var checksum = GetChecksum(dragonCurve);
+
+Console.WriteLine(checksum);
+
+// Part 2
+diskLength = 35651584;
+
+dragonCurve = GetDragonCurve();
+
+dragonCurve = dragonCurve.Substring(0, diskLength);
+
+checksum = GetChecksum(dragonCurve);
+
+Console.WriteLine(checksum);
+
+
+string GetDragonCurve()
+{
+	var result = input;
+
+	while (result.Length < diskLength)
+	{
+		result = GenerateDragonCurve(result);
+	}
+
+	return result;
+}
+
+string GenerateDragonCurve(string input)
+{
+	var a = input;
+	var b = new string(a.Reverse().Select(c => c == '0' ? '1' : '0').ToArray());
+
+	return a + "0" + b;
+}
+
+string GetChecksum(string input)
+{
+	var checksum = input;
+
+	while (checksum.Length % 2 == 0)
+	{
+		checksum = GenerateChecksum(checksum);
+	}
+
+	return checksum;
+}
+
+string GenerateChecksum(string input)
+{
+	var checksum = new StringBuilder();
+
+	for (int i = 0; i < input.Length; i += 2)
+	{
+		checksum.Append(input[i] == input[i + 1] ? '1' : '0');
+	}
+
+	return checksum.ToString();
+}
