dec9a565a053e6dc609ebb9174877ec68cdeb751
Jose Villa <jose.villa@version1.com>
2024-12-31 11:19:15 +0100

    2016-15

---
+++ 2016/15/Disc.cs
@@ -0,0 +1,16 @@
+﻿namespace _15
+{
+	public class Disc
+	{
+		public int Number { get; set; }
+		public int Positions { get; set; }
+		public int InitialPosition { get; set; }
+
+		public Disc(int number, int positions, int initialPosition)
+		{
+			Number = number;
+			Positions = positions;
+			InitialPosition = initialPosition;
+		}
+	}
+}
---
+++ 2016/15/Program.cs
@@ -0,0 +1,53 @@
+﻿using _15;
+
+var lines = File.ReadAllLinesAsync("Input.txt").Result;
+
+var discs = new List<Disc>();
+
+foreach (var line in lines)
+{
+	var parts = line.Split(' ');
+	var number = int.Parse(parts[1].Substring(1));
+	var positions = int.Parse(parts[3]);
+	var initialPosition = int.Parse(parts[^1].Trim('.'));
+
+	discs.Add(new Disc(number, positions, initialPosition));
+}
+Console.WriteLine(GetTime());
+
+// Part 2
+discs.Add(new Disc(7, 11, 0));
+
+Console.WriteLine(GetTime());
+
+
+int GetTime()
+{
+	int time = 0;
+
+	while (true)
+	{
+		var success = true;
+
+		for (int i = 0; i < discs.Count; i++)
+		{
+			var disc = discs[i];
+			var position = (disc.InitialPosition + time + i + 1) % disc.Positions;
+
+			if (position != 0)
+			{
+				success = false;
+				break;
+			}
+		}
+
+		if (success)
+		{
+			break;
+		}
+
+		time++;
+	}
+
+	return time;
+}
