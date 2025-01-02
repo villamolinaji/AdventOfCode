04e9e1af55d3993ff60ca2193d42045685e08316
Jose Villa <jose.villa@version1.com>
2025-01-02 14:43:54 +0100

    2017-09

---
+++ 2017/09/Program.cs
@@ -0,0 +1,55 @@
+﻿var input = File.ReadAllTextAsync("Input.txt").Result;
+
+var score = 0;
+var totalScore = 0;
+var scoreStack = new Stack<int>();
+bool isGarbage = false;
+
+int garbageCount = 0;
+
+for (int i = 0; i < input.Length; i++)
+{
+	if (input[i] == '!')
+	{
+		i++;
+		continue;
+	}
+
+	if (isGarbage)
+	{
+		if (input[i] == '>')
+		{
+			isGarbage = false;
+		}
+		else
+		{
+			garbageCount++;
+		}
+
+		continue;
+	}
+
+	if (!isGarbage)
+	{
+		if (input[i] == '<')
+		{
+			isGarbage = true;
+			continue;
+		}
+
+		if (input[i] == '{')
+		{
+			score++;
+			scoreStack.Push(score);
+		}
+		if (input[i] == '}')
+		{
+			totalScore += scoreStack.Pop();
+			score--;
+		}
+	}
+}
+
+Console.WriteLine(totalScore);
+
+Console.WriteLine(garbageCount);
