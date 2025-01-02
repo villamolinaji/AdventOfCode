74eb9bf48c4ed2f384caaac7b82a2287a48d1a94
Jose Villa <jose.villa@version1.com>
2025-01-02 11:27:16 +0100

    2017-05

---
+++ 2017/05/Program.cs
@@ -0,0 +1,40 @@
+﻿var lines = File.ReadAllLinesAsync("Input.txt").Result;
+
+var index = 0;
+var steps = 0;
+
+var instructions = lines.Select(int.Parse).ToList();
+
+while (index >= 0 &&
+	index < instructions.Count)
+{
+	var offset = instructions[index];
+
+	instructions[index] = offset + 1;
+
+	index += offset;
+	steps++;
+}
+
+Console.WriteLine(steps);
+
+// Part 2
+index = 0;
+steps = 0;
+
+instructions = lines.Select(int.Parse).ToList();
+
+while (index >= 0 &&
+	index < instructions.Count)
+{
+	var offset = instructions[index];
+
+	instructions[index] = offset >= 3
+		? offset - 1
+		: offset + 1;
+
+	index += offset;
+	steps++;
+}
+
+Console.WriteLine(steps);
