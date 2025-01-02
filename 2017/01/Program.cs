efbd84653139fa6ea9e98c73dc7cd4a9a706a50f
Jose Villa <jose.villa@version1.com>
2025-01-02 07:13:31 +0100

    2017-01

---
+++ 2017/01/Program.cs
@@ -0,0 +1,28 @@
+﻿var input = File.ReadAllTextAsync("Input.txt").Result;
+
+var result = 0;
+
+for (var i = 0; i < input.Length; i++)
+{
+	if (input[i] == input[(i + 1) % input.Length])
+	{
+		result += int.Parse(input[i].ToString());
+	}
+}
+
+Console.WriteLine(result);
+
+// Part 2
+result = 0;
+
+for (var i = 0; i < input.Length; i++)
+{
+	var nextIndex = (i + input.Length / 2) % input.Length;
+
+	if (input[i] == input[nextIndex])
+	{
+		result += int.Parse(input[i].ToString());
+	}
+}
+
+Console.WriteLine(result);
