428af46482c1b6a6cf7e4303913aa5d4e88770ef
Jose Villa <jose.villa@version1.com>
2025-01-03 16:52:10 +0100

    2017-18

---
+++ 2017/18/Machine.cs
@@ -0,0 +1,120 @@
+﻿namespace _18
+{
+	class Machine
+	{
+		private Dictionary<string, long> registers = new Dictionary<string, long>();
+		private int sendCount = 0;
+		private Queue<long> qIn;
+		private Queue<long> qOut;
+		private int index = 0;
+		private bool running;
+
+		public Machine(long p, Queue<long> qIn, Queue<long> qOut)
+		{
+			this["p"] = p;
+			this.qIn = qIn;
+			this.qOut = qOut;
+		}
+
+		private long this[string reg]
+		{
+			get => long.TryParse(reg, out var n)
+				? n
+				: registers.ContainsKey(reg)
+					? registers[reg]
+					: 0;
+			set => registers[reg] = value;
+		}
+
+		public IEnumerable<(bool running, int valueSent)> Execute(string[] lines)
+		{
+
+			while (index >= 0 && index < lines.Length)
+			{
+				running = true;
+				var line = lines[index];
+				var parts = line.Split(' ');
+				switch (parts[0])
+				{
+					case "snd":
+						snd(parts[1]);
+						break;
+					case "rcv":
+						rcv(parts[1]);
+						break;
+					case "set":
+						set(parts[1], parts[2]);
+						break;
+					case "add":
+						add(parts[1], parts[2]);
+						break;
+					case "mul":
+						mul(parts[1], parts[2]);
+						break;
+					case "mod":
+						mod(parts[1], parts[2]);
+						break;
+					case "jgz":
+						jgz(parts[1], parts[2]);
+						break;
+				}
+				yield return State();
+			}
+
+			running = false;
+
+			yield return State();
+		}
+
+		private (bool running, int valueSent) State() => (running, sendCount);
+
+		private void snd(string reg)
+		{
+			qOut.Enqueue(this[reg]);
+			sendCount++;
+			index++;
+		}
+
+		private void rcv(string reg)
+		{
+			if (qIn.Any())
+			{
+				this[reg] = qIn.Dequeue();
+				index++;
+			}
+			else
+			{
+				running = false;
+			}
+		}
+
+		private void set(string reg0, string reg1)
+		{
+			this[reg0] = this[reg1];
+			index++;
+		}
+
+		private void add(string reg0, string reg1)
+		{
+			this[reg0] += this[reg1];
+			index++;
+		}
+
+		private void mul(string reg0, string reg1)
+		{
+			this[reg0] *= this[reg1];
+			index++;
+		}
+
+		private void mod(string reg0, string reg1)
+		{
+			this[reg0] %= this[reg1];
+			index++;
+		}
+
+		private void jgz(string reg0, string reg1)
+		{
+			index += this[reg0] > 0 ? (int)this[reg1] : 1;
+		}
+	}
+}
---
+++ 2017/18/Program.cs
@@ -0,0 +1,78 @@
+﻿using _18;
+
+var lines = File.ReadAllLinesAsync("Input.txt").Result;
+
+var registers = new Dictionary<string, long>();
+
+long frequencyPlayed = 0;
+long index = 0;
+var endWhile = false;
+
+while (!endWhile)
+{
+	var line = lines[index];
+	var parts = line.Split(' ');
+
+	if (!registers.ContainsKey(parts[1]))
+	{
+		registers.Add(parts[1], 0);
+	}
+
+	switch (parts[0])
+	{
+		case "snd":
+			frequencyPlayed = registers[parts[1]];
+			break;
+		case "set":
+			registers[parts[1]] = int.TryParse(parts[2], out var value) ? value : registers[parts[2]];
+			break;
+		case "add":
+			registers[parts[1]] = registers[parts[1]] + (int.TryParse(parts[2], out value) ? value : registers[parts[2]]);
+			break;
+		case "mul":
+			registers[parts[1]] = registers[parts[1]] * (int.TryParse(parts[2], out value) ? value : registers[parts[2]]);
+			break;
+		case "mod":
+			registers[parts[1]] = registers[parts[1]] % (int.TryParse(parts[2], out value) ? value : registers[parts[2]]);
+			break;
+		case "rcv":
+			if (registers[parts[1]] != 0)
+			{
+				endWhile = true;
+			}
+			break;
+		case "jgz":
+			if (registers[parts[1]] > 0)
+			{
+				var offset = int.TryParse(parts[2], out value) ? value : registers[parts[2]];
+				long newIndex = index + offset - 1;
+
+				if (newIndex < 0 ||
+					newIndex >= lines.Length)
+				{
+					endWhile = true;
+				}
+				index = newIndex;
+			}
+			break;
+	}
+
+	index++;
+}
+
+Console.WriteLine(frequencyPlayed);
+
+// Part 2
+var q0 = new Queue<long>();
+var q1 = new Queue<long>();
+
+var timesSentProgram1 = Enumerable
+	.Zip(
+		new Machine(0, q0, q1).Execute(lines),
+		new Machine(1, q1, q0).Execute(lines),
+		(state0, state1) => (state0: state0, state1: state1))
+	.First(x => !x.state0.running && !x.state1.running)
+	.state1.valueSent;
+
+Console.WriteLine(timesSentProgram1);
+
