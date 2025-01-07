88f90a01696cefa26792ff0b6e63cd237b326a95
Jose Villa <jose.villa@version1.com>
2025-01-07 11:20:46 +0100

    2018-01

---
+++ 2018/01/Program.cs
@@ -0,0 +1,33 @@
+﻿var lines = File.ReadAllLinesAsync("Input.txt").Result;
+
+var frequency = 0;
+
+
+foreach (var line in lines)
+{
+	frequency += int.Parse(line);
+}
+
+Console.WriteLine(frequency);
+
+// Part 2
+var frequencies = new HashSet<int>();
+
+frequency = 0;
+frequencies.Add(frequency);
+
+var index = 0;
+
+while (true)
+{
+	frequency += int.Parse(lines[index]);
+
+	if (frequencies.Contains(frequency))
+	{
+		Console.WriteLine(frequency);
+		break;
+	}
+	frequencies.Add(frequency);
+
+	index = (index + 1) % lines.Length;
+}
