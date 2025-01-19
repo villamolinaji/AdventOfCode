b45f2fb830845060dfd7f0cc0ab668f817190eea
Jose Villa <jose.villa@version1.com>
2025-01-19 08:09:42 +0100

    2019-07

---
+++ 2019/07/IntcodeComputer.cs
@@ -0,0 +1,124 @@
+﻿namespace _07
+{
+	public class IntcodeComputer
+	{
+		private readonly long[] positions;
+
+		private int index;
+
+		public Queue<long> Input { get; } = new();
+
+		public Queue<long> Output { get; } = new();
+
+		public bool IsHalt { get; private set; }
+
+		public IntcodeComputer(long[] program)
+		{
+			positions = program.ToArray();
+			index = 0;
+		}
+
+		public void Run()
+		{
+			while (!IsHalt)
+			{
+				long instruction = positions[index];
+				long de = instruction % 100;
+				long c = (instruction / 100) % 10;
+				long b = (instruction / 1000) % 10;
+
+				long value1 = 0;
+				long value2 = 0;
+
+				switch (de)
+				{
+					case 1:
+						value1 = c == 0 ? positions[positions[index + 1]] : positions[index + 1];
+						value2 = b == 0 ? positions[positions[index + 2]] : positions[index + 2];
+
+						positions[positions[index + 3]] = value1 + value2;
+
+						index += 4;
+						break;
+					case 2:
+						value1 = c == 0 ? positions[positions[index + 1]] : positions[index + 1];
+						value2 = b == 0 ? positions[positions[index + 2]] : positions[index + 2];
+
+						positions[positions[index + 3]] = value1 * value2;
+						index += 4;
+						break;
+					case 3:
+						if (Input.Count == 0)
+						{
+							return;
+						}
+
+						positions[positions[index + 1]] = Input.Dequeue();
+						index += 2;
+
+						break;
+					case 4:
+						value1 = c == 0 ? positions[positions[index + 1]] : positions[index + 1];
+
+						Output.Enqueue(value1);
+
+						index += 2;
+
+						break;
+					case 5:
+						value1 = c == 0 ? positions[positions[index + 1]] : positions[index + 1];
+						value2 = b == 0 ? positions[positions[index + 2]] : positions[index + 2];
+
+						if (value1 != 0)
+						{
+							index = (int)value2;
+						}
+						else
+						{
+							index += 3;
+						}
+
+						break;
+					case 6:
+						value1 = c == 0 ? positions[positions[index + 1]] : positions[index + 1];
+						value2 = b == 0 ? positions[positions[index + 2]] : positions[index + 2];
+
+						if (value1 == 0)
+						{
+							index = (int)value2;
+						}
+						else
+						{
+							index += 3;
+						}
+
+						break;
+					case 7:
+						value1 = c == 0 ? positions[positions[index + 1]] : positions[index + 1];
+						value2 = b == 0 ? positions[positions[index + 2]] : positions[index + 2];
+
+						positions[positions[index + 3]] = value1 < value2 ? 1 : 0;
+
+						index += 4;
+
+						break;
+					case 8:
+						value1 = c == 0 ? positions[positions[index + 1]] : positions[index + 1];
+						value2 = b == 0 ? positions[positions[index + 2]] : positions[index + 2];
+
+						positions[positions[index + 3]] = value1 == value2 ? 1 : 0;
+
+						index += 4;
+
+						break;
+					case 99:
+						IsHalt = true;
+
+						return;
+					default:
+						throw new InvalidOperationException($"Unknown opcode {positions[index]}");
+				}
+			}
+		}
+	}
+}
---
+++ 2019/07/Program.cs
@@ -0,0 +1,94 @@
+﻿using _07;
+
+var input = File.ReadAllTextAsync("Input.txt").Result;
+
+var program = input.Split(',').Select(long.Parse).ToArray();
+
+var maxSignal = GetMaxSignal(new[] { 0, 1, 2, 3, 4 });
+
+Console.WriteLine(maxSignal);
+
+// Part 2
+maxSignal = GetMaxSignalWithFeedback(new[] { 5, 6, 7, 8, 9 });
+
+Console.WriteLine(maxSignal);
+
+
+long GetMaxSignal(int[] phaseSettings)
+{
+	return Permutations(phaseSettings).Max(phaseSequence => RunAmplifiers(phaseSequence));
+}
+
+long GetMaxSignalWithFeedback(int[] phaseSettings)
+{
+	return Permutations(phaseSettings).Max(phaseSequence => RunAmplifiersWithFeedback(phaseSequence));
+}
+
+long RunAmplifiers(int[] phaseSequence)
+{
+	long inputSignal = 0;
+
+	foreach (var phase in phaseSequence)
+	{
+		var intcode = new IntcodeComputer(program);
+
+		intcode.Input.Enqueue(phase);
+		intcode.Input.Enqueue(inputSignal);
+
+		intcode.Run();
+
+		inputSignal = intcode.Output.Dequeue();
+	}
+
+	return inputSignal;
+}
+
+long RunAmplifiersWithFeedback(int[] phaseSequence)
+{
+	var amplifiers = phaseSequence.Select(_ => new IntcodeComputer(program)).ToArray();
+
+	for (int i = 0; i < amplifiers.Length; i++)
+	{
+		amplifiers[i].Input.Enqueue(phaseSequence[i]);
+	}
+
+	long inputSignal = 0;
+	bool done = false;
+
+	while (!done)
+	{
+		for (int i = 0; i < amplifiers.Length; i++)
+		{
+			amplifiers[i].Input.Enqueue(inputSignal);
+
+			amplifiers[i].Run();
+
+			if (amplifiers[i].Output.Count > 0)
+			{
+				inputSignal = amplifiers[i].Output.Dequeue();
+			}
+
+			done = amplifiers[i].IsHalt && i == amplifiers.Length - 1;
+		}
+	}
+
+	return inputSignal;
+}
+
+IEnumerable<int[]> Permutations(int[] sequence)
+{
+	if (sequence.Length == 1)
+	{
+		yield return sequence;
+	}
+	else
+	{
+		for (int i = 0; i < sequence.Length; i++)
+		{
+			foreach (var perm in Permutations(sequence.Where((_, index) => index != i).ToArray()))
+			{
+				yield return new[] { sequence[i] }.Concat(perm).ToArray();
+			}
+		}
+	}
+}
