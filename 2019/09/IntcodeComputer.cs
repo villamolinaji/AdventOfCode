85fafa04d3753eece5d7b33e66b29757f36831bd
Jose Villa <jose.villa@version1.com>
2025-01-22 09:38:59 +0100

    2019-09

---
+++ 2019/09/IntcodeComputer.cs
@@ -0,0 +1,140 @@
+﻿namespace _09
+{
+	public class IntcodeComputer
+	{
+		private readonly Dictionary<long, long> memory;
+
+		private int index;
+
+		private long relativeBase;
+
+		public Queue<long> Input { get; } = new();
+
+		public Queue<long> Output { get; } = new();
+
+		public bool IsHalt { get; private set; }
+
+		public IntcodeComputer(long[] program)
+		{
+			memory = program
+				.Select((value, index) => new { value, index })
+				.ToDictionary(x => (long)x.index, x => x.value);
+			index = 0;
+			relativeBase = 0;
+		}
+
+		public void Run(int first)
+		{
+			Input.Enqueue(first);
+
+			while (!IsHalt)
+			{
+				int instruction = (int)GetMemory(index);
+				int opcode = instruction % 100;
+				int mode1 = (instruction / 100) % 10;
+				int mode2 = (instruction / 1000) % 10;
+				int mode3 = (instruction / 10000) % 10;
+
+
+				switch (opcode)
+				{
+					case 1:
+						SetParameter(3, mode3, GetParameter(1, mode1) + GetParameter(2, mode2));
+
+						index += 4;
+						break;
+					case 2:
+						SetParameter(3, mode3, GetParameter(1, mode1) * GetParameter(2, mode2));
+
+						index += 4;
+						break;
+					case 3:
+						if (Input.Count == 0)
+						{
+							return;
+						}
+
+						SetParameter(1, mode1, Input.Dequeue());
+
+						index += 2;
+
+						break;
+					case 4:
+						Output.Enqueue(GetParameter(1, mode1));
+
+						index += 2;
+
+						break;
+					case 5:
+						index = GetParameter(1, mode1) != 0 ? (int)GetParameter(2, mode2) : index + 3;
+
+						break;
+					case 6:
+						index = GetParameter(1, mode1) == 0 ? (int)GetParameter(2, mode2) : index + 3;
+
+						break;
+					case 7:
+						SetParameter(3, mode3, GetParameter(1, mode1) < GetParameter(2, mode2) ? 1 : 0);
+
+						index += 4;
+
+						break;
+					case 8:
+						SetParameter(3, mode3, GetParameter(1, mode1) == GetParameter(2, mode2) ? 1 : 0);
+
+						index += 4;
+
+						break;
+					case 9:
+						relativeBase += GetParameter(1, mode1);
+
+						index += 2;
+
+						break;
+					case 99:
+						IsHalt = true;
+
+						return;
+					default:
+						throw new InvalidOperationException($"Unknown opcode {opcode}");
+				}
+			}
+		}
+
+		private long GetMemory(long address) => memory.ContainsKey(address) ? memory[address] : 0;
+
+		private void SetMemory(long address, long value)
+		{
+			memory[address] = value;
+		}
+
+		private long GetParameter(long offset, int mode)
+		{
+			long value = GetMemory(index + offset);
+
+			switch (mode)
+			{
+				case 0:
+					return GetMemory(value);
+				case 1:
+					return value;
+				case 2:
+					return GetMemory(relativeBase + value);
+			}
+
+			return 0;
+		}
+
+		private void SetParameter(long offset, int mode, long value)
+		{
+			long address = GetMemory(index + offset);
+
+			if (mode == 2)
+			{
+				address += relativeBase;
+			}
+
+			SetMemory(address, value);
+		}
+	}
+}
---
+++ 2019/09/Program.cs
@@ -0,0 +1,18 @@
+﻿using _09;
+
+var input = File.ReadAllTextAsync("Input.txt").Result;
+
+var program = input.Split(',').Select(long.Parse).ToArray();
+
+var computer = new IntcodeComputer(program);
+
+computer.Run(1);
+
+Console.WriteLine(computer.Output.Dequeue());
+
+// Part 2
+computer = new IntcodeComputer(program);
+
+computer.Run(2);
+
+Console.WriteLine(computer.Output.Dequeue());
