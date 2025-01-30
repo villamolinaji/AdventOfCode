d24dc62062b02bf252ebcbb937b484a4667d5488
Jose Villa <jose.villa@version1.com>
2025-01-30 16:08:27 +0100

    2019-25

---
+++ 25/IntcodeComputer.cs
@@ -0,0 +1,194 @@
+﻿using System.Text;
+
+namespace _25
+{
+	public class IntcodeComputer
+	{
+		private Dictionary<long, long> memory;
+
+		private int index;
+
+		private long relativeBase;
+
+		public Queue<long> Input { get; set; } = new();
+
+		public Queue<long> Output { get; } = new();
+
+		public bool IsHalt { get; private set; }
+
+		public IntcodeComputer(long[] program)
+		{
+			memory = program
+				.Select((value, index) => new { value, index })
+				.ToDictionary(x => (long)x.index, x => x.value);
+			index = 0;
+			relativeBase = 0;
+		}
+
+		public IntcodeComputer()
+		{
+			memory = new Dictionary<long, long>();
+			index = 0;
+			relativeBase = 0;
+		}
+
+		public IntcodeComputer Clone()
+		{
+			var cloneComputer = new IntcodeComputer();
+
+			cloneComputer.memory = new Dictionary<long, long>(memory);
+			cloneComputer.index = index;
+			cloneComputer.relativeBase = relativeBase;
+			cloneComputer.Input = new Queue<long>(Input);
+
+			return cloneComputer;
+		}
+
+		public void Run(params string?[] input)
+		{
+			Run(AsciiEncode(string.Join("", from line in input select line + "\n")));
+		}
+
+		public void Run(long first)
+		{
+			Run(new[] { first });
+		}
+
+		public void Run(long[] input)
+		{
+			foreach (var i in input)
+			{
+				this.Input.Enqueue(i);
+			}
+
+			this.Output.Clear();
+
+			while (!IsHalt)
+			{
+				int instruction = (int)GetMemory(index);
+				int opcode = instruction % 100;
+				int mode1 = (instruction / 100) % 10;
+				int mode2 = (instruction / 1000) % 10;
+				int mode3 = (instruction / 10000) % 10;
+
+
+				switch (opcode)
+				{
+					case 1:
+						SetParameter(3, mode3, GetParameter(1, mode1) + GetParameter(2, mode2));
+
+						index += 4;
+						break;
+					case 2:
+						SetParameter(3, mode3, GetParameter(1, mode1) * GetParameter(2, mode2));
+
+						index += 4;
+						break;
+					case 3:
+						if (Input.Count == 0)
+						{
+							return;
+						}
+
+						SetParameter(1, mode1, Input.Dequeue());
+
+						index += 2;
+
+						break;
+					case 4:
+						Output.Enqueue(GetParameter(1, mode1));
+
+						index += 2;
+
+						break;
+					case 5:
+						index = GetParameter(1, mode1) != 0 ? (int)GetParameter(2, mode2) : index + 3;
+
+						break;
+					case 6:
+						index = GetParameter(1, mode1) == 0 ? (int)GetParameter(2, mode2) : index + 3;
+
+						break;
+					case 7:
+						SetParameter(3, mode3, GetParameter(1, mode1) < GetParameter(2, mode2) ? 1 : 0);
+
+						index += 4;
+
+						break;
+					case 8:
+						SetParameter(3, mode3, GetParameter(1, mode1) == GetParameter(2, mode2) ? 1 : 0);
+
+						index += 4;
+
+						break;
+					case 9:
+						relativeBase += GetParameter(1, mode1);
+
+						index += 2;
+
+						break;
+					case 99:
+						IsHalt = true;
+
+						return;
+					default:
+						throw new InvalidOperationException($"Unknown opcode {opcode}");
+				}
+			}
+		}
+
+		public string ToAscii()
+		{
+			var result = new StringBuilder();
+			while (this.Output.Count > 0)
+			{
+				var value = this.Output.Dequeue();
+
+				result.Append((char)value);
+			}
+
+			return result.ToString();
+		}
+
+		private long GetMemory(long address) => memory.ContainsKey(address) ? memory[address] : 0;
+
+		private void SetMemory(long address, long value)
+		{
+			memory[address] = value;
+		}
+
+		private long GetParameter(long offset, int mode)
+		{
+			long value = GetMemory(index + offset);
+
+			switch (mode)
+			{
+				case 0:
+					return GetMemory(value);
+				case 1:
+					return value;
+				case 2:
+					return GetMemory(relativeBase + value);
+			}
+
+			return 0;
+		}
+
+		private void SetParameter(long offset, int mode, long value)
+		{
+			long address = GetMemory(index + offset);
+
+			if (mode == 2)
+			{
+				address += relativeBase;
+			}
+
+			SetMemory(address, value);
+		}
+
+		private static long[] AsciiEncode(string st)
+		{
+			return (from ch in st select (long)ch).ToArray();
+		}
+	}
+}
---
+++ 25/Program.cs
@@ -0,0 +1,156 @@
+﻿using System.Text.RegularExpressions;
+using _25;
+
+var input = File.ReadAllTextAsync("Input.txt").Result;
+
+var directions = new List<string>()
+{
+	"south",
+	"east",
+	"west",
+	"north"
+};
+
+var securityRoom = "== Security Checkpoint ==";
+
+var program = input.Split(',').Select(long.Parse).ToArray();
+
+var computer = new IntcodeComputer(program);
+
+computer.Run();
+
+var description = computer.ToAscii();
+
+VisitRooms(securityRoom, computer, description, args =>
+{
+	foreach (var item in args.items)
+	{
+		if (item != "infinite loop")
+		{
+			var takeCmd = "take " + item;
+			var clone = computer.Clone();
+			clone.Run(takeCmd);
+			if (!clone.IsHalt && Inventory(clone).Contains(item))
+			{
+				computer.Run(takeCmd);
+			}
+		}
+	}
+
+	return null;
+});
+
+var door = VisitRooms(
+	securityRoom,
+	computer,
+	description,
+	args => (args.room! == securityRoom)
+		? args.doors.Single(door => door != ReverseDir(args.doorTaken))
+		: null);
+
+Random random = new Random();
+
+var inventory = Inventory(computer).ToList();
+var floor = new List<string>();
+
+var password = string.Empty;
+
+while (true)
+{
+	computer.Run(door);
+	var output = computer.ToAscii();
+
+	if (output.Contains("heavier"))
+	{
+		TakeOrDrop("take", floor, inventory);
+	}
+	else if (output.Contains("lighter"))
+	{
+		TakeOrDrop("drop", inventory, floor);
+	}
+	else
+	{
+		password = Regex.Match(output, @"\d+").Value;
+
+		break;
+	}
+}
+
+Console.WriteLine(password);
+
+
+void TakeOrDrop(string cmd, List<string> from, List<string> to)
+{
+	var i = random.Next(from.Count);
+	var item = from[i];
+	from.RemoveAt(i);
+	to.Add(item);
+	computer.Run(cmd + " " + item);
+}
+
+string ReverseDir(string direction) => directions[3 - directions.IndexOf(direction)];
+
+string? VisitRooms(
+	string securityRoom,
+	IntcodeComputer icm,
+	string description,
+	Func<(IEnumerable<string> items, string room, string? doorTaken, IEnumerable<string> doors), string> callback)
+{
+	var roomsSeen = new HashSet<string>();
+
+	return DFS(description, null, roomsSeen, callback);
+}
+
+string? DFS(
+	string description,
+	string? doorTaken,
+	HashSet<string> roomsSeen,
+	Func<(IEnumerable<string> items, string room, string? doorTaken, IEnumerable<string> doors), string> callback)
+{
+	var room = description.Split("\n").Single(x => x.Contains("=="));
+	var listing = GetListItems(description).ToHashSet();
+	var doors = listing.Intersect(directions);
+	var items = listing.Except(doors);
+
+	if (!roomsSeen.Contains(room))
+	{
+		roomsSeen.Add(room);
+
+		var res = callback((items, room, doorTaken, doors));
+
+		if (res != null)
+		{
+			return res;
+		}
+
+		if (room != securityRoom)
+		{
+			foreach (var door in doors)
+			{
+				computer.Run(door);
+				res = DFS(computer.ToAscii(), door, roomsSeen, callback);
+				if (res != null)
+				{
+					return res;
+				}
+				computer.Run(ReverseDir(door));
+			}
+		}
+	}
+
+	return null;
+}
+
+IEnumerable<string> Inventory(IntcodeComputer icm)
+{
+	icm.Run("inv");
+
+	return GetListItems(icm.ToAscii());
+}
+
+IEnumerable<string> GetListItems(string description)
+{
+	return description.Split("\n")
+		.Where(l => l.StartsWith("- "))
+		.Select(line => line.Substring(2));
+}
