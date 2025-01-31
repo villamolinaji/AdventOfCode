b5cdd5d638a65518c096a0357ef32f626c8a11b6
Jose Villa <jose.villa@version1.com>
2025-01-31 14:59:35 +0100

    2020-06

---
+++ 2020/06/Program.cs
@@ -0,0 +1,37 @@
+﻿var lines = File.ReadAllLinesAsync("Input.txt").Result;
+
+var groups = new List<List<string>>();
+
+var persons = new List<string>();
+
+foreach (var line in lines)
+{
+	if (string.IsNullOrWhiteSpace(line))
+	{
+		groups.Add(persons);
+		persons = new List<string>();
+	}
+	else
+	{
+		persons.Add(line);
+	}
+}
+
+groups.Add(persons);
+
+var sum = 0;
+
+var sum2 = 0;
+
+foreach (var group in groups)
+{
+	var answers = group.SelectMany(x => x).Distinct().ToList();
+	sum += answers.Count;
+
+	var answers2 = answers.Where(x => group.All(y => y.Contains(x))).ToList();
+	sum2 += answers2.Count;
+}
+
+Console.WriteLine(sum);
+
+Console.WriteLine(sum2);
