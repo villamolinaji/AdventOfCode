113fbf1d9c979dbcf5a0ecb49afe90225904ef8f
Jose Villa <jose.villa@version1.com>
2025-02-12 13:45:58 +0100

    2021-06

---
+++ 2021/06/Program.cs
@@ -0,0 +1,26 @@
+﻿var input = File.ReadAllTextAsync("Input.txt").Result;
+
+
+
+Console.WriteLine(CalculateFished(80));
+
+//Part 2
+Console.WriteLine(CalculateFished(256));
+
+
+long CalculateFished(int days)
+{
+	var fishCount = new long[9];
+
+	foreach (var fish in input.Split(',').Select(x => int.Parse(x)))
+	{
+		fishCount[fish]++;
+	}
+
+	for (var i = 0; i < days; i++)
+	{
+		fishCount[(i + 7) % 9] += fishCount[i % 9];
+	}
+
+	return fishCount.Sum();
+}
