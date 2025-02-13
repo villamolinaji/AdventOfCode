f892fe23f81b105194833b443d5e3255e255cc4f
Jose Villa <jose.villa@version1.com>
2025-02-13 12:39:38 +0100

    2020-25

---
+++ 2020/25/Program.cs
@@ -0,0 +1,29 @@
+﻿var doorPublicKey = 13135480;
+var cardPublicKey = 8821721;
+
+var mod = 20201227;
+var pow = 0;
+var subject = 7L;
+var encriptionKey = subject;
+
+while (encriptionKey != doorPublicKey && encriptionKey != cardPublicKey)
+{
+	encriptionKey = (encriptionKey * subject) % mod;
+
+	pow++;
+}
+
+subject = encriptionKey == doorPublicKey
+	? cardPublicKey
+	: doorPublicKey;
+
+encriptionKey = subject;
+
+while (pow > 0)
+{
+	encriptionKey = (encriptionKey * subject) % mod;
+
+	pow--;
+}
+
+Console.WriteLine(encriptionKey);
